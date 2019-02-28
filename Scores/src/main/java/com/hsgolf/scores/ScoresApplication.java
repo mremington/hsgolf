a43ef6f60cc22bde3753a5256109a06ee62328f4
mike <mike@DESKTOP-H98JHIB>
2019-02-27 19:25:42 -0800

    upload hsgolf

---
+++ Scores/src/main/java/com/hsgolf/scores/ScoresApplication.java
@@ -0,0 +1,13 @@
+package com.hsgolf.scores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoresApplication.class, args);
+	}
+
+}
